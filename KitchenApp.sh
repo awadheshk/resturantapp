caca82cd1ce3521021e9528230f458995cb36695
awadheshk <54056550+awadheshk@users.noreply.github.com>
2025-05-23 12:36:27 +0530

    Update KitchenApp.sh

--- KitchenApp.sh
+++ KitchenApp.sh
@@ -34,25 +34,25 @@ source genaikitchenapp/bin/activate
 python3 -m  pip install -r requirements.txt
 
 # Step 5: Start Streamlit application
-echo "Running Kitchen Recipe Generator application in the background...${RESET}"
-nohup streamlit run chef.py \
-  --browser.serverAddress=localhost \
-  --server.enableCORS=false \
-  --server.enableXsrfProtection=false \
-  --server.port 8080 > kitchenrecipe.log 2>&1 &
+# echo "Running Kitchen Recipe Generator application in the background...${RESET}"
+# nohup streamlit run chef.py \
+#  --browser.serverAddress=localhost \
+#  --server.enableCORS=false \
+#  --server.enableXsrfProtection=false \
+#  --server.port 8080 > kitchenrecipe.log 2>&1 &
 
-# Step 11: Create Artifact Repository
+# Step 6: Create Artifact Repository
 
 echo "Creating Artifact Registry repository...${RESET}"
 AR_REPO='kitchen-recipe-generator-repo'
 SERVICE_NAME='kitchen-recipe-generator-app' 
 gcloud artifacts repositories create "$AR_REPO" --location="$GCP_REGION" --repository-format=Docker
 
-# Step 12: Submit Cloud Build
+# Step 7: Submit Cloud Build
 echo "Submitting Cloud Build...${RESET}"
 gcloud builds submit --tag "$GCP_REGION-docker.pkg.dev/$GCP_PROJECT/$AR_REPO/$SERVICE_NAME"
 
-# Step 13: Deploy Cloud Run Service
+# Step 8: Deploy Cloud Run Service
 echo "Deploying Cloud Run service...${RESET}"
 gcloud run deploy "$SERVICE_NAME" \
   --port=8080 \
@@ -63,12 +63,12 @@ gcloud run deploy "$SERVICE_NAME" \
   --project=$GCP_PROJECT \
   --set-env-vars=GCP_PROJECT=$GCP_PROJECT,GCP_REGION=$GCP_REGION
 
-# Step 14: Get Cloud Run Service URL
+# Step 9: Get Cloud Run Service URL
 echo "Fetching Cloud Run service URL...${RESET}"
 CLOUD_RUN_URL=$(gcloud run services describe "$SERVICE_NAME" --region="$GCP_REGION" --format='value(status.url)')
 
 echo
-echo "Kitchen Recipe Generator is running at: ${RESET}""http://localhost:8080"
-echo
+#echo "Kitchen Recipe Generator is running at: ${RESET}""http://localhost:8080"
+#echo
 echo "Cloud Run Service is available at: ${RESET}""$CLOUD_RUN_URL"
 echo
